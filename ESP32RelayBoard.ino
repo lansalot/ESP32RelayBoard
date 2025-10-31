20c4a5f7340cef45972750f768372fc91d88932c
lansalot <lansalot@gmail.com>
2025-10-31 18:58:35 +0000

    refactor

--- ESP32RelayBoard.ino
+++ ESP32RelayBoard.ino
@@ -3,7 +3,7 @@ int clockPin = 13; // Clock pin of 74HC595 is connected to Digital pin 6
 int dataPin = 14;  // Data pin of 74HC595 is connected to Digital pin 4
 int oePin = 5;     // oePin - not using this
 
-uint16_t Data;
+uint16_t relayState = 0; // Stores the current state of all 16 relays
 
 void setup()
 {
@@ -12,201 +12,50 @@ void setup()
       pinMode(dataPin, OUTPUT);
       pinMode(clockPin, OUTPUT);
       pinMode(oePin, OUTPUT);
+      
+      // Initialize all relays to OFF
+      updateRelays();
+}
+
+// Function to set relay state (relay number 1-16, state true=ON/false=OFF)
+void setRelay(int relayNum, bool state)
+{
+      if (relayNum < 1 || relayNum > 16) return; // Validate relay number
+      
+      int bitPos = relayNum - 1; // Convert to 0-based index
+      
+      if (state) {
+            relayState |= (1 << bitPos);  // Turn relay ON
+      } else {
+            relayState &= ~(1 << bitPos); // Turn relay OFF
+      }
+      
+      updateRelays();
+}
+
+// Function to update the shift registers with current relay state
+void updateRelays()
+{
+      digitalWrite(latchPin, LOW);
+      shiftOut(dataPin, clockPin, MSBFIRST, (relayState >> 8));
+      shiftOut(dataPin, clockPin, MSBFIRST, relayState);
+      digitalWrite(latchPin, HIGH);
 }
 
 void loop()
 {
-
-      Data = 0b0000000000000001;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000000000000011;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000000000000111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000000000001111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000000000011111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000000000111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000000001111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000000011111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000000111111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000001111111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000011111111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000111111111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0001111111111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0011111111111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0111111111111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111111111111;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111111111110;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111111111100;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111111111000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111111110000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111111100000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111111000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111110000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111100000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111111000000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111110000000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111100000000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1111000000000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1110000000000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1100000000000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b1000000000000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
-      Data = 0b0000000000000000;
-      digitalWrite(latchPin, LOW);
-      shiftOut(dataPin, clockPin, MSBFIRST, (Data >> 8));
-      shiftOut(dataPin, clockPin, MSBFIRST, Data);
-      digitalWrite(latchPin, HIGH);
-      delay(100);
+      // Example: Turn on relays one by one, then turn them all off
+      for (int i = 1; i <= 16; i++) {
+            setRelay(i, true);  // Turn relay ON
+            delay(100);
+      }
+      
+      for (int i = 1; i <= 16; i++) {
+            setRelay(i, false);  // Turn relay OFF
+            delay(100);
+      }
+      
+      // You can also control individual relays:
+      // setRelay(5, true);   // Turn relay 5 ON
+      // setRelay(12, false); // Turn relay 12 OFF
 }
